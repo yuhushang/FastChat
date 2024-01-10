0d07b637f39a1c7d1056319025f34ccc287d692e
huijiawu0 <3816911+huijiawu0@users.noreply.github.com>
2024-01-10 09:58:40 +0800

    1

--- fastchat/serve/flask/app_5004.py
+++ fastchat/serve/flask/app_5004.py
@@ -70,7 +70,6 @@ def get_modelpage_detail():
     for model, scores in ability_scores.items():
         model_scores = {"model": model}
         model_scores.update(scores)
-        # 将这个新字典添加到列表中
         ability_scores_array.append(model_scores)
     result = {
         "request_id": request_id,
@@ -88,6 +87,20 @@ def get_datapage_list():
     request_id = random_uuid()
     result = DATA_JSON.copy()
     result.update({"request_id": request_id})
+    return json.dumps(result, ensure_ascii=False)
+
+
+@app.route('/get_datapage_category', methods=['POST'])
+def get_datapage_category():
+    request_id = random_uuid()
+    result = {}
+    result.update({"通用伦理": DATA_JSON.copy})
+    result.update({"基础能力": {""}})
+    result.update({"抗干扰性": {""}})
+    result.update({"request_id": request_id})
+    resulta = []
+    for k, v in result.items():
+        resulta.append(v)
     return json.dumps(result, ensure_ascii=False)
 
 
